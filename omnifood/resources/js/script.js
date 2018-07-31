0c4b78a47b1033999df4e7eeab860463a2437364
Loic El Thesea <loic.elthesea@outlook.com>
2018-07-31 04:05:53 +0200

    Add navigation with scrolling effect

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+  var scrollTo = function(target) {
+    $('html,body').animate({scrollTop: target.offset().top}, 1000);
+  };
+
+  // scroll on buttons
+  $('.js--scroll-to-plans').click(function() {
+    scrollTo($('.js--section-plans'));
+  });
+
+  $('.js--scroll-to-start').click(function() {
+    scrollTo($('.js--section-features'));
+  });
+
+  // Navigation scroll
+  $('a[href*=\\#]:not([href=\\#])').click(function() {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      scrollTo(target);
+      return false;
+    }
+  });
+
+  // sticky navigation
   $('.js--section-features').waypoint(
     function(direction) {
       if (direction == 'down') {
